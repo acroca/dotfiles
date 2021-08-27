ff8501b3e87418fa76ccced7bbb62f6bc5176d53
Albert Callarisa Roca <albert@Alberts-MacBook-Pro.local>
2021-08-27 14:14:58 +0200

    moving to mac

--- run_once_74-install-alacritty.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt install alacritty
--- run_once_76-install-notion.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/nativefier
-docker run --rm -v ~/nativefier:/target nativefier/nativefier --name notion https://www.notion.so/ /target/
