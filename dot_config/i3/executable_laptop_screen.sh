401312635a4813752fda4c1332b95ec54cb9dc28
Albert Callarisa <acallarisa@polygon.technology>
2023-05-10 12:12:01 +0200

    updates

--- dot_config/i3/executable_laptop_screen.sh
+++ dot_config/i3/executable_laptop_screen.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# # Just internal
-xrandr --output eDP-1-1 --mode 0x1e4 --primary --output DP-1-4-2 --off
+xrandr --output eDP-1-1 --mode 3456x2160 --primary --output DP-1-4-2 --off
--- dot_config/i3/executable_screens.sh
+++ dot_config/i3/executable_screens.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# # Just external
-xrandr --output eDP-1-1 --off --output DP-1-4-2 --mode 0x217 --primary
-
-# # Just internal
-# xrandr --output eDP-1-1 --mode 0x1e4 --primary --output DP-1-4-2 --off
+xrandr --output eDP-1-1 --off --output DP-1-4-2  --mode 3440x1440 -r 59.97 --primary
