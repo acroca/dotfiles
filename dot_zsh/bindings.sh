7b2ddec2c30d597e455faca28c0075b91d20267e
Albert Callarisa <albert.callarisa@form3.tech>
2024-12-18 16:41:59 +0100

    Update from Form3

--- dot_zsh/bindings.sh
+++ dot_zsh/bindings.sh
@@ -2,13 +2,13 @@ bindkey -e
 bindkey -M emacs '^[[1;5C' forward-word
 bindkey -M emacs '^[[1;5D' backward-word
 
-autoload -U up-line-or-beginning-search
-zle -N up-line-or-beginning-search
-bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
+# autoload -U up-line-or-beginning-search
+# zle -N up-line-or-beginning-search
+# bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
 
-autoload -U down-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
+# autoload -U down-line-or-beginning-search
+# zle -N down-line-or-beginning-search
+# bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
 
 bindkey '^[[A' history-search-backward
 bindkey '^[[B' history-search-forward
