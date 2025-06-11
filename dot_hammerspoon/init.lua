3ab2e1509dc2f731eb11c2bf434c69efd1b8144c
Albert Callarisa <albert@diagrid.io>
2025-06-11 18:11:48 +0200

    Refactor window positioning functions and update hotkey bindings in Hammerspoon configuration

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,46 +1,43 @@
 hs.window.animationDuration = 0
 
--- local pushWindow = function(win, x, y, w, h)
---   return function()
---     local f = win:frame()
---     local screen = win:screen()
---     local max = screen:frame()
+local push = function(x, y, w, h)
+  return function()
+    local win = hs.window.focusedWindow()
 
---     f.x = max.x + (max.w * x)
---     f.y = max.y + (max.h * y)
---     f.w = max.w * w
---     f.h = max.h * h
---     win:setFrame(f)
---   end
--- end
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
--- local push = function(x, y, w, h)
---   return function()
---     local win = hs.window.focusedWindow()
---     pushWindow(win, x, y, w, h)()
---   end
--- end
+    f.x = max.x + (max.w * x)
+    f.y = max.y + (max.h * y)
+    f.w = max.w * w
+    f.h = max.h * h
 
--- hs.hotkey.bind({"alt", "ctrl"}, "left", push(0, 0, 0.5, 1))
--- hs.hotkey.bind({"alt", "ctrl", "cmd"}, "left", push(0, 0, 0.7, 1))
--- hs.hotkey.bind({"alt", "ctrl"}, "up", push(0, 0, 1, 0.5))
--- hs.hotkey.bind({"alt", "ctrl"}, "right", push(0.5, 0, 0.5, 1))
--- hs.hotkey.bind({"alt", "ctrl", "cmd"}, "right", push(0.7, 0, 0.3, 1))
--- hs.hotkey.bind({"alt", "ctrl"}, "down", push(0, 0.5, 1, 0.5))
+    -- Hack: https://github.com/Hammerspoon/hammerspoon/issues/3224#issuecomment-1294359070
+    local axApp = hs.axuielement.applicationElement(win:application())
+    local wasEnhanced = axApp.AXEnhancedUserInterface
+    if wasEnhanced then
+      axApp.AXEnhancedUserInterface = false
+    end
+    win:setFrame(f) -- or win:moveToScreen(someScreen), etc.
+    if wasEnhanced then
+      axApp.AXEnhancedUserInterface = true
+    end
+  end
+end
 
--- hs.hotkey.bind({"alt", "ctrl"}, "u", push(0, 0, 0.5, 0.5))
--- hs.hotkey.bind({"alt", "ctrl"}, "i", push(0.5, 0, 0.5, 0.5))
--- hs.hotkey.bind({"alt", "ctrl"}, "k", push(0.5, 0.5, 0.5, 0.5))
--- hs.hotkey.bind({"alt", "ctrl"}, "j", push(0, 0.5, 0.5, 0.5))
+hs.hotkey.bind({"alt", "ctrl"}, "return", push(0, 0, 1, 1))
+hs.hotkey.bind({"alt", "ctrl"}, "left", push(0, 0, 1/2, 1))
+hs.hotkey.bind({"alt", "ctrl"}, "right", push(1/2, 0, 1/2, 1))
+hs.hotkey.bind({"alt", "ctrl"}, "up", push(0, 0, 1, 1/2))
+hs.hotkey.bind({"alt", "ctrl"}, "down", push(0, 1/2, 1, 1/2))
 
--- -- Fullscreen
--- hs.hotkey.bind({"alt", "ctrl"}, "m", push(0, 0, 1, 1))
--- -- Center of the screen with border both ways
--- hs.hotkey.bind({"alt", "ctrl"}, "n", push(0.2, 0.1, 0.6, 0.8))
--- -- Center of the screen, full height
--- hs.hotkey.bind({"alt", "ctrl", "shift"}, "n", push(0.2, 0, 0.6, 1))
--- -- Bottom left corner, good size for sized-screenshare (in the external screen)
--- hs.hotkey.bind({"alt", "ctrl"}, "p", push(0, 0.3, 0.5, 0.7))
+hs.hotkey.bind({"alt", "ctrl"}, "t", push(1/3, 0, 2/3, 1))
+hs.hotkey.bind({"alt", "ctrl"}, "e", push(0, 0, 2/3, 1))
+
+hs.hotkey.bind({"alt", "ctrl"}, "d", push(0, 0, 1/3, 1))
+hs.hotkey.bind({"alt", "ctrl"}, "f", push(1/3, 0, 1/3, 1))
+hs.hotkey.bind({"alt", "ctrl"}, "g", push(2/3, 0, 1/3, 1))
 
 
 hs.hotkey.bind({"cmd"}, "e", function()
@@ -60,9 +57,9 @@ hs.hotkey.bind({"cmd"}, "3", function()
   hs.application.launchOrFocus("Firefox")
 end)
 hs.hotkey.bind({"cmd"}, "4", function()
-  -- if hs.application.frontmostApplication():title() == "Slack" then
-    -- hs.application.launchOrFocus("WhatsApp")
-  -- else
+  if hs.application.frontmostApplication():title() == "Slack" then
+    hs.application.launchOrFocus("Discord")
+  else
     hs.application.launchOrFocus("Slack")
-  -- end
+  end
 end)
